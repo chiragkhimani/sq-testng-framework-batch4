a0865bef90a9dc8c347ef39861e98d5d9e6e47b9
chiragkhimani <khimanichirag@gmail.com>
2021-11-12 06:02:28 +0530

    Cucumber Step definition

---
+++ src/test/java/com/automation/steps/LoginSteps.java
@@ -0,0 +1,23 @@
+package com.automation.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+	@Given("user is on website")
+	public void user_is_on_website() {
+
+	}
+
+	@When("user login with valid cred")
+	public void user_login_with_valid_cred() {
+
+	}
+
+	@Given("verify login successful")
+	public void verify_login_successful() {
+
+	}
+
+}
